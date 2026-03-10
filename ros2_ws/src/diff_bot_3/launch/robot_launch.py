a7a63ba477e7370347625874b1f6c5d16d7b3642
Johan Sebastian Castiblanco Galeano <j.castiblancog@uniandes.edu.co>
2026-03-10 11:31:56 -0500

    Update package name to 'diff_bot_3' in launch file

---
+++ ros2_ws/src/diff_bot_3/launch/robot_launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. El Cerebro (Hardware real)
+        Node(
+            package='diff_bot_3',
+            executable='robot_core',
+            name='robot_core_node',
+            output='screen'
+        ),
+        # 2. El Reproductor (Servicio)
+        Node(
+            package='diff_bot_3',
+            executable='robot_player',
+            name='player_diferencial',
+            output='screen'
+        ),
+        # 3. La Interfaz Gráfica
+        Node(
+            package='diff_bot_3',
+            executable='robot_interface',
+            name='interface_diferencial',
+            output='screen',
+            emulate_tty=True # Crucial para que funcionen los print() y los input()
+        ),
+        # 4. El Control por Teclado
+        Node(
+            package='diff_bot_3',
+            executable='robot_teleop',
+            name='teleop_node',
+            output='screen',
+            emulate_tty=True # Crucial para leer el teclado con pynput
+        )
+    ])
